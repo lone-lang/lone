4160bd7eeec21bf2a8ce1fcc8b7a47c6e60db916
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-02-12 19:35:47 -0300

    aarch64: define all arity system call functions
    This is a lot of boilerplate but it helps the compiler generate
    less code so it's worth it.

--- arch/aarch64.c
+++ arch/aarch64.c
@@ -10,7 +10,117 @@
  **/
 
 static long
-system_call(long n, long _1, long _2, long _3, long _4, long _5, long _6)
+system_call_0(long n)
+{
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0");
+
+	__asm__ volatile
+	("svc 0"
+
+		: "=r" (x0)
+		:  "r" (x8)
+		: "cc", "memory");
+
+	return x0;
+}
+
+static long
+system_call_1(long n, long _1)
+{
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = _1;
+
+	__asm__ volatile
+	("svc 0"
+
+		: "+r" (x0)
+		:  "r" (x8)
+		: "cc", "memory");
+
+	return x0;
+}
+
+static long
+system_call_2(long n, long _1, long _2)
+{
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = _1;
+	register long x1 __asm__("x1") = _2;
+
+	__asm__ volatile
+	("svc 0"
+
+		: "+r" (x0)
+		: "r" (x1),
+		  "r" (x8)
+		: "cc", "memory");
+
+	return x0;
+}
+
+static long
+system_call_3(long n, long _1, long _2, long _3)
+{
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = _1;
+	register long x1 __asm__("x1") = _2;
+	register long x2 __asm__("x2") = _3;
+
+	__asm__ volatile
+	("svc 0"
+
+		: "+r" (x0)
+		: "r" (x1), "r" (x2),
+		  "r" (x8)
+		: "cc", "memory");
+
+	return x0;
+}
+
+static long
+system_call_4(long n, long _1, long _2, long _3, long _4)
+{
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = _1;
+	register long x1 __asm__("x1") = _2;
+	register long x2 __asm__("x2") = _3;
+	register long x3 __asm__("x3") = _4;
+
+	__asm__ volatile
+	("svc 0"
+
+		: "+r" (x0)
+		: "r" (x1), "r" (x2), "r" (x3),
+		  "r" (x8)
+		: "cc", "memory");
+
+	return x0;
+}
+
+static long
+system_call_5(long n, long _1, long _2, long _3, long _4, long _5)
+{
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = _1;
+	register long x1 __asm__("x1") = _2;
+	register long x2 __asm__("x2") = _3;
+	register long x3 __asm__("x3") = _4;
+	register long x4 __asm__("x4") = _5;
+
+	__asm__ volatile
+	("svc 0"
+
+		: "+r" (x0)
+		: "r" (x1), "r" (x2), "r" (x3), "r" (x4),
+		  "r" (x8)
+		: "cc", "memory");
+
+	return x0;
+}
+
+static long
+system_call_6(long n, long _1, long _2, long _3, long _4, long _5, long _6)
 {
 	register long x8 __asm__("x8") = n;
 	register long x0 __asm__("x0") = _1;
@@ -24,8 +134,8 @@ system_call(long n, long _1, long _2, long _3, long _4, long _5, long _6)
 	("svc 0"
 
 		: "+r" (x0)
-		: "r" (x1), "r" (x2), "r" (x3),
-		  "r" (x4), "r" (x5), "r" (x8)
+		: "r" (x1), "r" (x2), "r" (x3), "r" (x4), "r" (x5),
+		  "r" (x8)
 		: "cc", "memory");
 
 	return x0;
