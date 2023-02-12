22ea0f4ee20ad509d2cf6f38432224f5639ed8ae
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-02-12 00:27:36 -0300

    aarch64: define system_call function
    System calls on aarch64 use:
    
     - x0 - x5 for arguments
     - x8 for system call number
     - x0 for return value

--- arch/aarch64.c
+++ arch/aarch64.c
@@ -7,6 +7,27 @@
  *
  **/
 
+long system_call(long n, long _1, long _2, long _3, long _4, long _5, long _6)
+{
+	register long x8 __asm__("x8") = n;
+	register long x0 __asm__("x0") = _1;
+	register long x1 __asm__("x1") = _2;
+	register long x2 __asm__("x2") = _3;
+	register long x3 __asm__("x3") = _4;
+	register long x4 __asm__("x4") = _5;
+	register long x5 __asm__("x5") = _6;
+
+	__asm__ volatile
+	("svc 0"
+
+		: "+r" (x0)
+		: "r" (x1), "r" (x2), "r" (x3),
+		  "r" (x4), "r" (x5), "r" (x8)
+		: "cc", "memory");
+
+	return x0;
+}
+
 /**
  *
  * initial stack layout - logical
