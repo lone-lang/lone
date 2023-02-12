cfedda99508af13128fa767b82647c0e8d0ca19a
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-02-11 23:49:56 -0300

    aarch64: document initial process stack layout
    Linux uses the stack to pass data to processes before execution:
    
     - Arguments count
     - Arguments array
     - Environment variables array
     - Auxiliary values array
    
    Document the initial state of the stack for future reference.

--- arch/aarch64.c
+++ arch/aarch64.c
@@ -6,3 +6,21 @@
  * system-call:     x0 = "svc 0" [x8] x0 x1 x2 x3 x4 x5
  *
  **/
+
+/**
+ *
+ * initial stack layout - logical
+ *
+ * sp → 0                         | argc
+ *      1                         | argv
+ *      argv + *argc + 1          | envp
+ *      &(*envp++ == 0) + 1       | auxv
+ *
+ * initial stack layout - bytes
+ *
+ * sp → 0                         | argc
+ *      8                         | argv
+ *      argv + 8 * (*argc + 1)    | envp
+ *      &(*envp++ == 0) + 8       | auxv
+ *
+ **/
