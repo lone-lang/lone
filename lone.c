53cbfd6d7f9a4950e80d3a1dc521ec972a766a34
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-02-19 17:57:41 -0300

    lone: print the auxiliary vector as a lisp list
    The auxiliary vector is now a list of key-value pairs. Print it!

--- lone.c
+++ lone.c
@@ -728,6 +728,10 @@ long lone(int argc, unsigned char **argv, unsigned char **envp, struct auxiliary
 	lone_print(&lone, environment, 1);
 	linux_write(1, "\n\n", 2);
 
+	linux_write(1, "Auxiliary values: ", sizeof("Auxiliary values: ") - 1);
+	lone_print(&lone, auxiliary_values, 1);
+	linux_write(1, "\n\n", 2);
+
 	lone_print(&lone, lone_evaluate(&lone, lone_read(&lone, 0)), 1);
 	linux_write(1, "\n", 1);
 
