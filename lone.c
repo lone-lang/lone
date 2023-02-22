b47aae2961914fc2de1c05f7da6442abb0b26854
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-02-21 23:57:17 -0300

    lone: define memory move function
    This function will copy bytes from one memory block to another while
    accounting for possible overlap. Not the most efficient version...
    It works nicely though.

--- lone.c
+++ lone.c
@@ -107,6 +107,20 @@ static void lone_lisp_initialize(struct lone_lisp *lone, unsigned char *memory, 
 	lone->values = 0;
 }
 
+static void lone_memory_move(void *from, void *to, size_t count)
+{
+	unsigned char *source = from, *destination = to;
+
+	if (source >= destination) {
+		/* destination is at or behind source, copy forwards */
+		while (count--) { *destination++ = *source++; }
+	} else {
+		/* destination is ahead of source, copy backwards */
+		source += count; destination += count;
+		while (count--) { *--destination = *--source; }
+	}
+}
+
 static void lone_memory_split(struct lone_memory *block, size_t used)
 {
 	size_t excess = block->size - used;
