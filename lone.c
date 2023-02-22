e7b8c6314bab890ddc28941d5ab84319154bf633
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-02-21 22:25:34 -0300

    lone: coalesce freed block with its surroundings
    Whenever the program deallocates memory, check if surrounding blocks are
    also free and merge them all into a single big block if they are.

--- lone.c
+++ lone.c
@@ -160,6 +160,9 @@ static void lone_deallocate(struct lone_lisp *lone, void * pointer)
 {
 	struct lone_memory *block = ((struct lone_memory *) pointer) - 1;
 	block->free = 1;
+
+	lone_memory_coalesce(block);
+	lone_memory_coalesce(block->prev);
 }
 
 static struct lone_value *lone_value_create(struct lone_lisp *lone)
