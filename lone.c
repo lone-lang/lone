0a94d905ed54b7a12cfa54b218d1231fdf8ec63c
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-02-23 11:02:44 -0300

    lone: define lone table creation function
    Allocates memory for a lone table of the specified capacity.

--- lone.c
+++ lone.c
@@ -281,6 +281,22 @@ static struct lone_value *lone_list_create_nil(struct lone_lisp *lone)
 	return lone_list_create(lone, 0, 0);
 }
 
+static struct lone_value *lone_table_create(struct lone_lisp *lone, size_t capacity)
+{
+	struct lone_value *value = lone_value_create(lone);
+	value->type = LONE_TABLE;
+	value->table.capacity = capacity;
+	value->table.count = 0;
+	value->table.entries = lone_allocate(lone, capacity * sizeof(*value->table.entries));
+
+	for (size_t i = 0; i < capacity; ++i) {
+		value->table.entries[i].key = 0;
+		value->table.entries[i].value = 0;
+	}
+
+	return value;
+}
+
 static struct lone_value *lone_integer_create(struct lone_lisp *lone, long integer)
 {
 	struct lone_value *value = lone_value_create(lone);
