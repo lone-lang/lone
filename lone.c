2e6798261808f6cd77607bbdc1781253e09b6e35
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-13 13:25:15 -0300

    lone: accept a variable number of lists in import
    This allows programmers to avoid repetition by importing everything
    they need with a single import call.
    
    Turns this:
    
        (import lone)
        (import linux system-call)
    
    Into this:
    
        (import (lone) (linux system-call))
    
    Much better!
    
    Also update all the tests to match the new import calling convention.

--- lone.c
+++ lone.c
@@ -403,7 +403,7 @@ static void lone_lisp_initialize(struct lone_lisp *lone, unsigned char *memory, 
 	lone->values = 0;
 	lone->symbol_table = lone_table_create(lone, 256, 0);
 	lone->modules.loaded = lone_table_create(lone, 32, 0);
-	struct lone_function_flags import_flags = { .evaluate_arguments = 0, .evaluate_result = 0, .variable_arguments = 0 };
+	struct lone_function_flags import_flags = { .evaluate_arguments = 0, .evaluate_result = 0, .variable_arguments = 1 };
 	lone->modules.import = lone_primitive_create(lone, "import", lone_primitive_import, 0, import_flags);
 	lone->modules.null = lone_module_create(lone, 0);
 }
@@ -1733,37 +1733,44 @@ static struct lone_value *lone_primitive_divide(struct lone_lisp *lone, struct l
    ╰────────────────────────────────────────────────────────────────────────╯ */
 static struct lone_value *lone_primitive_import(struct lone_lisp *lone, struct lone_value *closure, struct lone_value *environment, struct lone_value *arguments)
 {
-	struct lone_value *name, *module, *value;
+	struct lone_value *argument, *name, *module, *value;
 
 	if (lone_is_nil(arguments)) { /* nothing to import: (import) */ linux_exit(-1); }
-	name = lone_list_first(arguments);
-	if (name->type != LONE_SYMBOL) { /* module name not a symbol: (import 10) */ linux_exit(-1); }
-	module = lone_table_get(lone, lone->modules.loaded, name);
-	if (lone_is_nil(module)) { /* module not found: (import non-existent) */ linux_exit(-1); }
-	arguments = lone_list_rest(arguments);
 
-	if (lone_is_nil(arguments)) {
-		/* full import, bind all symbols: (import module) */
-		struct lone_table_entry *entries = module->module.environment->table.entries;
-		size_t i, capacity = module->module.environment->table.capacity;
-		for (i = 0; i < capacity; ++i) {
-			if (entries[i].key) {
-				lone_table_set(lone, environment, entries[i].key, entries[i].value);
+	for (/* argument */; !lone_is_nil(arguments); arguments = lone_list_rest(arguments)) {
+		argument = lone_list_first(arguments);
+		if (argument->type != LONE_LIST) { /* not an import list: (import module) */ linux_exit(-1); }
+
+		if (lone_is_nil(argument)) { /* nothing to import: (import ()) */ linux_exit(-1); }
+		name = lone_list_first(argument);
+		if (name->type != LONE_SYMBOL) { /* module name not a symbol: (import (10)) */ linux_exit(-1); }
+		module = lone_table_get(lone, lone->modules.loaded, name);
+		if (lone_is_nil(module)) { /* module not found: (import (non-existent)) */ linux_exit(-1); }
+		argument = lone_list_rest(argument);
+
+		if (lone_is_nil(argument)) {
+			/* full import, bind all symbols: (import (module)) */
+			struct lone_table_entry *entries = module->module.environment->table.entries;
+			size_t i, capacity = module->module.environment->table.capacity;
+			for (i = 0; i < capacity; ++i) {
+				if (entries[i].key) {
+					lone_table_set(lone, environment, entries[i].key, entries[i].value);
+				}
 			}
+		} else {
+			/* limited import, bind only specified symbols: (import (module x f)) */
+			do {
+				name = lone_list_first(argument);
+				if (name->type != LONE_SYMBOL) { /* name not a symbol: (import (module 10)) */ linux_exit(-1); }
+
+				value = lone_table_get(lone, module->module.environment, name);
+				if (lone_is_nil(value)) { /* name not set in module */ linux_exit(-1); }
+
+				lone_table_set(lone, environment, name, value);
+
+				argument = lone_list_rest(argument);
+			} while (!lone_is_nil(argument));
 		}
-	} else {
-		/* limited import, bind only specified symbols: (import module x f) */
-		do {
-			name = lone_list_first(arguments);
-			if (name->type != LONE_SYMBOL) { /* name not a symbol: (import 10) */ linux_exit(-1); }
-
-			value = lone_table_get(lone, module->module.environment, name);
-			if (lone_is_nil(value)) { /* name not set in module */ linux_exit(-1); }
-
-			lone_table_set(lone, environment, name, value);
-
-			arguments = lone_list_rest(arguments);
-		} while (!lone_is_nil(arguments));
 	}
 
 	return module;
