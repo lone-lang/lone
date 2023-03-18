9dc2128e4f60ab02e6b2e9470741d55b0289430b
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-18 01:38:49 -0300

    lone: move forward declaration further down
    Puts it closer to the function that needs it.

--- lone.c
+++ lone.c
@@ -523,8 +523,6 @@ static struct lone_value *lone_nil(struct lone_lisp *lone)
 	return lone->constants.nil;
 }
 
-static struct lone_value *lone_intern_c_string(struct lone_lisp *, char *);
-
 static struct lone_value *lone_function_create(struct lone_lisp *lone, struct lone_value *arguments, struct lone_value *code, struct lone_value *environment, struct lone_function_flags flags)
 {
 	struct lone_value *value = lone_value_create(lone);
@@ -535,6 +533,8 @@ static struct lone_value *lone_function_create(struct lone_lisp *lone, struct lo
 	value->function.flags = flags;
 	return value;
 }
+
+static struct lone_value *lone_intern_c_string(struct lone_lisp *, char *);
 
 static struct lone_value *lone_primitive_create(struct lone_lisp *lone, char *name, lone_primitive function, struct lone_value *closure, struct lone_function_flags flags)
 {
