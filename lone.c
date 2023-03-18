a14e4b50d9d25929b51b2858abac33e8783bde7f
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-18 00:29:14 -0300

    lone: describe details of the memory/GC system
    I had to read a lot of material to understand and implement this stuff.
    Better write down what I learned while it's still fresh in my mind.
    Maybe it will help someone. A future me, most probably.

--- lone.c
+++ lone.c
@@ -208,6 +208,25 @@ struct lone_lisp {
    │    They will be split into smaller units when allocated                │
    │    and merged together with free neighbors when deallocated.           │
    │                                                                        │
+   │    Memory blocks are segments prefixed by a block descriptor           │
+   │    that tracks its size, allocation status as well as pointers         │
+   │    to surrounding memory blocks.                                       │
+   │                                                                        │
+   │    Lone employs a very simple mark-and-sweep garbage collector.        │
+   │    It starts by marking all values reachable by the interpreter        │
+   │    in its current state. Then it walks the list of all values,         │
+   │    deallocating any unmarked object it finds as well as any            │
+   │    memory they happen own based on the value's type.                   │
+   │                                                                        │
+   │    Like memory blocks, lone values are prefixed by a header            │
+   │    structure containing metadata such as its marked state              │
+   │    as well as a pointer to the next object in the list.                │
+   │                                                                        │
+   │    Since these headers are prefixed to pointers returned by            │
+   │    allocation and value creation functions, it is simple to            │
+   │    calculate their locations given a pointer to a memory block         │
+   │    or lone value: simply subtract the header's size from it.           │
+   │                                                                        │
    ╰────────────────────────────────────────────────────────────────────────╯ */
 struct lone_memory {
 	struct lone_memory *prev, *next;
