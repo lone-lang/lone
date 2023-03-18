887cbb6baefa9c7c91e331a01e52d8f20d57d741
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-18 02:10:02 -0300

    lone: comment the lone table initializer
    Document the table implementation and the algorithms chosen,
    as well as the built-in prototype system.

--- lone.c
+++ lone.c
@@ -594,6 +594,20 @@ static struct lone_value *lone_vector_create(struct lone_lisp *lone, size_t capa
 	return value;
 }
 
+/* ╭────────────────────────────────────────────────────────────────────────╮
+   │                                                                        │
+   │    Lone tables are openly addressed, linearly probed hash tables.      │
+   │    Currently, lone tables use the FNV-1a hashing algorithm.            │
+   │    They also strive to maintain a load factor of at most 0.5:          │
+   │    tables will be rehashed once they're above half capacity.           │
+   │    They do not use tombstones to delete keys.                          │
+   │                                                                        │
+   │    Tables are able to inherit from another table: missing keys         │
+   │    are also looked up in the parent table. This is currently used      │
+   │    to implement nested environments but will also serve as a           │
+   │    prototype-based object system as in Javascript and Self.            │
+   │                                                                        │
+   ╰────────────────────────────────────────────────────────────────────────╯ */
 static struct lone_value *lone_table_create(struct lone_lisp *lone, size_t capacity, struct lone_value *prototype)
 {
 	struct lone_value *value = lone_value_create(lone);
