7dad546b07f87ad802fa07d5b81345360774146d
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-18 01:16:28 -0300

    lone: comment on the lone list initializers
    Document the semantics of each parameter and of nil.

--- lone.c
+++ lone.c
@@ -488,6 +488,22 @@ static struct lone_value *lone_bytes_create(struct lone_lisp *lone, unsigned cha
 	return lone_bytes_transfer(lone, copy, count);
 }
 
+/* ╭────────────────────────────────────────────────────────────────────────╮
+   │                                                                        │
+   │    Lone lists are pairs of lone values: first and rest.                │
+   │    Usually first is the element while rest is another pair.            │
+   │    This explains their names. Also called car and cdr.                 │
+   │                                                                        │
+   │    Although often the case, rest need not be another pair.             │
+   │    Any other object may be set: (1 . 2); first = 1, rest = 2.          │
+   │    So rest could also be named second.                                 │
+   │                                                                        │
+   │    A list with null first and rest pointers is known as nil.           │
+   │    It is provided as a constant by the lone interpreter.               │
+   │    Their presence in the rest of a list marks its end.                 │
+   │    New nil values may be created by C code that builds lists.          │
+   │                                                                        │
+   ╰────────────────────────────────────────────────────────────────────────╯ */
 static struct lone_value *lone_list_create(struct lone_lisp *lone, struct lone_value *first, struct lone_value *rest)
 {
 	struct lone_value *value = lone_value_create(lone);
