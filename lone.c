e3524f98e410d2e9d04dd189ef0018f0be057e47
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-18 01:39:48 -0300

    lone: comment the lone function initializers
    Document functions and primitives, their structure and application.

--- lone.c
+++ lone.c
@@ -523,6 +523,30 @@ static struct lone_value *lone_nil(struct lone_lisp *lone)
 	return lone->constants.nil;
 }
 
+/* ╭────────────────────────────────────────────────────────────────────────╮
+   │                                                                        │
+   │    Lone functions represent a body of executable lone lisp code.       │
+   │    They have a list of argument names to be bound during function      │
+   │    application, a list of expressions to be evaluated when called      │
+   │    and a closure: a reference to the environment it was defined in.    │
+   │                                                                        │
+   │    To apply a function is to create a new environment with its         │
+   │    argument names bound to the given arguments and then evaluate       │
+   │    the function's expressions in the context of that environment.      │
+   │                                                                        │
+   │    The function flags control how the function is applied.             │
+   │    It may be configured to receive evaluated or unevaluated            │
+   │    arguments as well as to evaluate the result automatically.          │
+   │    These features allow code manipulation and generation.              │
+   │    It may also be configured to be variadic: all arguments             │
+   │    are collected into a list and passed as a single argument.          │
+   │                                                                        │
+   │    Primitives are lone functions implemented in C.                     │
+   │    They are always variadic and must check their arguments.            │
+   │    All of them must follow the primitive function prototype.           │
+   │    They also have closures which are pointers to arbitrary data.       │
+   │                                                                        │
+   ╰────────────────────────────────────────────────────────────────────────╯ */
 static struct lone_value *lone_function_create(struct lone_lisp *lone, struct lone_value *arguments, struct lone_value *code, struct lone_value *environment, struct lone_function_flags flags)
 {
 	struct lone_value *value = lone_value_create(lone);
