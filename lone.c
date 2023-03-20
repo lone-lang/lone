b601ff4f55e21453abc5c3f0e76e9a95bc614c0a
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-20 02:18:40 -0300

    lone: generalize concatenate into a join function
    The join function supports adding separators between elements.
    Concatenation is just joining with a null separator,
    so implement concatenate on top of join.

--- lone.c
+++ lone.c
@@ -986,10 +986,10 @@ static inline int lone_bytes_equals_c_string(struct lone_bytes bytes, char *c_st
 	return lone_bytes_equals(bytes, c_string_bytes);
 }
 
-static struct lone_bytes lone_concatenate(struct lone_lisp *lone, struct lone_value *arguments, lone_predicate is_valid)
+static struct lone_bytes lone_join(struct lone_lisp *lone, struct lone_value *separator, struct lone_value *arguments, lone_predicate is_valid)
 {
 	struct lone_value *head, *argument;
-	unsigned char *concatenated;
+	unsigned char *joined;
 	size_t total = 0, position = 0;
 
 	if (!is_valid) { is_valid = lone_has_bytes; }
@@ -998,23 +998,44 @@ static struct lone_bytes lone_concatenate(struct lone_lisp *lone, struct lone_va
 		/* invalid predicate function */ linux_exit(-1);
 	}
 
+	if (separator && !lone_is_nil(separator)) {
+		if (!is_valid(separator)) { linux_exit(-1); }
+	}
+
 	for (head = arguments; !lone_is_nil(head); head = lone_list_rest(head)) {
 		argument = lone_list_first(head);
 		if (!is_valid(argument)) { linux_exit(-1); }
+
 		total += argument->bytes.count;
+		if (separator && !lone_is_nil(separator)) {
+			if (!lone_is_nil(lone_list_rest(head))) { total += separator->bytes.count; }
+		}
 	}
 
-	concatenated = lone_allocate(lone, total + 1);
+	joined = lone_allocate(lone, total + 1);
 
 	for (head = arguments; !lone_is_nil(head); head = lone_list_rest(head)) {
 		argument = lone_list_first(head);
-		lone_memory_move(argument->bytes.pointer, concatenated + position, argument->bytes.count);
+
+		lone_memory_move(argument->bytes.pointer, joined + position, argument->bytes.count);
 		position += argument->bytes.count;
+
+		if (separator && !lone_is_nil(separator)) {
+			if (!lone_is_nil(lone_list_rest(head))) {
+				lone_memory_move(separator->bytes.pointer, joined + position, separator->bytes.count);
+				position += separator->bytes.count;
+			}
+		}
 	}
 
-	concatenated[total] = '\0';
+	joined[total] = '\0';
 
-	return (struct lone_bytes) { .count = total, .pointer = concatenated };
+	return (struct lone_bytes) { .count = total, .pointer = joined };
+}
+
+static struct lone_bytes lone_concatenate(struct lone_lisp *lone, struct lone_value *arguments, lone_predicate is_valid)
+{
+	return lone_join(lone, 0, arguments, is_valid);
 }
 
 static void lone_vector_resize(struct lone_lisp *lone, struct lone_value *vector, size_t new_capacity)
