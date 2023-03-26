003f22ffa72a85d22f949790e75670f7f264fb1e
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-03-26 04:13:15 -0300

    lone: save registers on stack before marking
    This lets the garbage collector find roots stored in registers.

--- lone.c
+++ lone.c
@@ -521,6 +521,9 @@ static void lone_find_and_mark_stack_roots(struct lone_lisp *lone)
 
 static void lone_mark_all_reachable_values(struct lone_lisp *lone)
 {
+	lone_registers registers;                /* stack space for registers */
+	lone_save_registers(registers);          /* spill registers on stack */
+
 	lone_mark_known_roots(lone);             /* precise */
 	lone_find_and_mark_stack_roots(lone);    /* conservative */
 }
