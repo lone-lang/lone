b2c188a36913cbe46d84822a65ff414d06bdf0d7
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-04-06 03:56:34 -0300

    lone: define the greater than comparator
    Checks the types of the arguments before comparing.

--- lone.c
+++ lone.c
@@ -1141,6 +1141,14 @@ static bool lone_integer_is_less_than_or_equal_to(struct lone_value *x, struct l
 	else { return false; }
 }
 
+static bool lone_integer_is_greater_than(struct lone_value *x, struct lone_value *y)
+{
+	if (!(lone_is_integer(x) && lone_is_integer(y))) { /* can't compare non-integers */ linux_exit(-1); }
+
+	if (x->integer > y->integer) { return true; }
+	else { return false; }
+}
+
 /* ╭────────────────────────────────────────────────────────────────────────╮
    │                                                                        │
    │    List manipulation functions.                                        │
