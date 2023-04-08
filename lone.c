ef799d3a6d29e3313aec6b7c46d7dfdcabf6501d
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-04-08 04:14:12 -0300

    lone: define the text predicate primitive
    Returns true if the given value is a text.

--- lone.c
+++ lone.c
@@ -2695,6 +2695,11 @@ static struct lone_value *lone_primitive_is_symbol(struct lone_lisp *lone, struc
 	return lone_apply_predicate(lone, arguments, lone_is_symbol);
 }
 
+static struct lone_value *lone_primitive_is_text(struct lone_lisp *lone, struct lone_value *closure, struct lone_value *environment, struct lone_value *arguments)
+{
+	return lone_apply_predicate(lone, arguments, lone_is_text);
+}
+
 static struct lone_value *lone_apply_comparator(struct lone_lisp *lone, struct lone_value *arguments, lone_comparator function)
 {
 	struct lone_value *argument, *next;
@@ -3808,6 +3813,14 @@ static void lone_builtin_module_lone_initialize(struct lone_lisp *lone)
 	                     lone_primitive_create(lone,
 	                                           "is_symbol",
 	                                           lone_primitive_is_symbol,
+	                                           module,
+	                                           (struct lone_function_flags) { 1, 0, 1 }));
+
+	lone_table_set(lone, module->module.environment,
+	                     lone_intern_c_string(lone, "text?"),
+	                     lone_primitive_create(lone,
+	                                           "is_text",
+	                                           lone_primitive_is_text,
 	                                           module,
 	                                           (struct lone_function_flags) { 1, 0, 1 }));
 
