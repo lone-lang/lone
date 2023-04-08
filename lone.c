c8b6bb8776f797b04a24ce59db33947cb1504ed2
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-04-08 04:07:51 -0300

    lone: define the table predicate primitive
    Returns true if the given value is a table.

--- lone.c
+++ lone.c
@@ -2685,6 +2685,11 @@ static struct lone_value *lone_primitive_is_vector(struct lone_lisp *lone, struc
 	return lone_apply_predicate(lone, arguments, lone_is_vector);
 }
 
+static struct lone_value *lone_primitive_is_table(struct lone_lisp *lone, struct lone_value *closure, struct lone_value *environment, struct lone_value *arguments)
+{
+	return lone_apply_predicate(lone, arguments, lone_is_table);
+}
+
 static struct lone_value *lone_apply_comparator(struct lone_lisp *lone, struct lone_value *arguments, lone_comparator function)
 {
 	struct lone_value *argument, *next;
@@ -3782,6 +3787,14 @@ static void lone_builtin_module_lone_initialize(struct lone_lisp *lone)
 	                     lone_primitive_create(lone,
 	                                           "is_vector",
 	                                           lone_primitive_is_vector,
+	                                           module,
+	                                           (struct lone_function_flags) { 1, 0, 1 }));
+
+	lone_table_set(lone, module->module.environment,
+	                     lone_intern_c_string(lone, "table?"),
+	                     lone_primitive_create(lone,
+	                                           "is_table",
+	                                           lone_primitive_is_table,
 	                                           module,
 	                                           (struct lone_function_flags) { 1, 0, 1 }));
 
