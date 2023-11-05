9ef5ee10db77d69e302ca60ab6a4fe60e0dea274
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-11-05 19:33:49 -0300

    lone: move memory structures to source header
    They can now be included separately if necessary.

--- lone.c
+++ lone.c
@@ -45,51 +45,6 @@ static ssize_t __attribute__((fd_arg_write(1), tainted_args)) linux_write(int fd
 	return system_call_3(__NR_write, fd, (long) buffer, (long) count);
 }
 
-/* ╭────────────────────┨ LONE LISP MEMORY ALLOCATION ┠─────────────────────╮
-   │                                                                        │
-   │    Lone is designed to work without any dependencies except Linux,     │
-   │    so it does not make use of even the system's C library.             │
-   │    In order to bootstrap itself in such harsh conditions,              │
-   │    it must be given some memory to work with.                          │
-   │                                                                        │
-   │    Lone manages its own memory with a block-based allocator.           │
-   │    Memory blocks are allocated on a first fit basis.                   │
-   │    They will be split into smaller units when allocated                │
-   │    and merged together with free neighbors when deallocated.           │
-   │                                                                        │
-   │    Memory blocks are segments prefixed by a block descriptor           │
-   │    that tracks its size, allocation status as well as pointers         │
-   │    to surrounding memory blocks.                                       │
-   │                                                                        │
-   │    Lone employs a very simple mark-and-sweep garbage collector.        │
-   │    It starts by marking all values reachable by the interpreter        │
-   │    in its current state. Then it walks the list of all values,         │
-   │    deallocating any unmarked object it finds as well as any            │
-   │    memory they happen own based on the value's type.                   │
-   │                                                                        │
-   │    Like memory blocks, lone values are prefixed by a header            │
-   │    structure containing metadata such as its marked state              │
-   │    as well as a pointer to the next object in the list.                │
-   │                                                                        │
-   │    Since these headers are prefixed to pointers returned by            │
-   │    allocation and value creation functions, it is simple to            │
-   │    calculate their locations given a pointer to a memory block         │
-   │    or lone value: simply subtract the header's size from it.           │
-   │                                                                        │
-   ╰────────────────────────────────────────────────────────────────────────╯ */
-struct lone_memory {
-	struct lone_memory *prev, *next;
-	int free;
-	size_t size;
-	unsigned char pointer[];
-};
-
-struct lone_heap {
-	struct lone_heap *next;
-	size_t count;
-	struct lone_value values[];
-};
-
 static void lone_memory_move(void *from, void *to, size_t count)
 {
 	unsigned char *source = from, *destination = to;
