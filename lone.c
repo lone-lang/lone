71f84964166829e9079ceb069383b62016e6bc58
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-11-05 09:38:52 -0300

    lone: move type enumeration to source header
    It can now be included separately if necessary.

--- lone.c
+++ lone.c
@@ -46,38 +46,6 @@ static ssize_t __attribute__((fd_arg_write(1), tainted_args)) linux_write(int fd
 
 #include <lone/definitions.h>
 
-/* ╭──────────────────────────┨ LONE LISP TYPES ┠───────────────────────────╮
-   │                                                                        │
-   │    Lone implements dynamic data types as a tagged union.               │
-   │    Supported types are:                                                │
-   │                                                                        │
-   │        ◦ Module       the isolated programming environment type        │
-   │        ◦ Function     the reusable executable expressions type         │
-   │        ◦ Primitive    the built-in C subroutine type                   │
-   │        ◦ List         the linked list and tree type                    │
-   │        ◦ Vector       the contiguous array of values type              │
-   │        ◦ Table        the hash table, prototype and object type        │
-   │        ◦ Symbol       the keyword and interned string type             │
-   │        ◦ Text         the UTF-8 encoded text type                      │
-   │        ◦ Bytes        the binary data and low level string type        │
-   │        ◦ Integer      the signed integer type                          │
-   │        ◦ Pointer      the memory addressing and dereferencing type     │
-   │                                                                        │
-   ╰────────────────────────────────────────────────────────────────────────╯ */
-enum lone_type {
-	LONE_MODULE,
-	LONE_FUNCTION,
-	LONE_PRIMITIVE,
-	LONE_LIST,
-	LONE_VECTOR,
-	LONE_TABLE,
-	LONE_SYMBOL,
-	LONE_TEXT,
-	LONE_BYTES,
-	LONE_INTEGER,
-	LONE_POINTER,
-};
-
 struct lone_list {
 	struct lone_value *first;
 	struct lone_value *rest;
--- source/include/lone/structures.h
+++ source/include/lone/structures.h
@@ -1,6 +1,7 @@
 #ifndef LONE_STRUCTURES_SOURCE_HEADER
 #define LONE_STRUCTURES_SOURCE_HEADER
 
+#include <lone/enum/type.h>
 #include <lone/struct/bytes.h>
 
 #endif /* LONE_STRUCTURES_SOURCE_HEADER */
