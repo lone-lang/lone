e0c3c5b47ebc045c84ded1d31dd8dd0d8f8dd92c
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-11-05 06:45:03 -0300

    lone: place definitions into their own header
    The lone source code has gotten pretty big.
    Time to gradually split it into separate files.
    This will also allow me to better document them.

--- lone.c
+++ lone.c
@@ -46,38 +46,7 @@ static ssize_t __attribute__((fd_arg_write(1), tainted_args)) linux_write(int fd
 	return system_call_3(__NR_write, fd, (long) buffer, (long) count);
 }
 
-/* ╭────────────────────────────────────────────────────────────────────────╮
-   │                                                                        │
-   │                                      bits = 32    |    bits = 64       │
-   │    digits = ceil(bits * log10(2)) =  10           |    20              │
-   │                                                                        │
-   │    https://en.wikipedia.org/wiki/FNV_hash                              │
-   │    https://datatracker.ietf.org/doc/draft-eastlake-fnv/                │
-   │                                                                        │
-   ╰────────────────────────────────────────────────────────────────────────╯ */
-#if __BITS_PER_LONG == 64
-	#define DECIMAL_DIGITS_PER_LONG 20
-	#define FNV_PRIME 0x00000100000001B3UL
-	#define FNV_OFFSET_BASIS 0xCBF29CE484222325UL
-#elif __BITS_PER_LONG == 32
-	#define DECIMAL_DIGITS_PER_LONG 10
-	#define FNV_PRIME 0x01000193UL
-	#define FNV_OFFSET_BASIS 0x811C9DC5
-#else
-	#error "Unsupported architecture"
-#endif
-
-#ifndef LONE_BUFFER_SIZE
-	#define LONE_BUFFER_SIZE 4096
-#endif
-
-#ifndef LONE_MEMORY_SIZE
-	#define LONE_MEMORY_SIZE (1024 * 1024)
-#endif
-
-#ifndef LONE_ALIGNMENT
-	#define LONE_ALIGNMENT 16
-#endif
+#include <lone/definitions.h>
 
 /* ╭──────────────────────────┨ LONE LISP TYPES ┠───────────────────────────╮
    │                                                                        │
