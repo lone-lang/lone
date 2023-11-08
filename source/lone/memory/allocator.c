f3b99dff635e28dbcfba77a4f38a9733e0ba7d92
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-11-07 23:50:28 -0300

    lone: fold align functions into memory allocator
    It's related to the memory allocator routines.
    Place them in the same source file.

--- source/lone/memory/allocator.c
+++ source/lone/memory/allocator.c
@@ -4,6 +4,24 @@
 #include <lone/struct/lisp.h>
 #include <lone/struct/memory.h>
 
+static size_t __attribute__((const)) lone_next_power_of_2(size_t n)
+{
+	size_t next = 1;
+	while (next < n) { next *= 2; }
+	return next;
+}
+
+static size_t __attribute__((const)) lone_next_power_of_2_multiple(size_t n, size_t m)
+{
+	m = lone_next_power_of_2(m);
+	return (n + m - 1) & (~(m - 1));
+}
+
+size_t __attribute__((const)) lone_align(size_t size, size_t alignment)
+{
+	return lone_next_power_of_2_multiple(size, alignment);
+}
+
 void lone_memory_split(struct lone_memory *block, size_t used)
 {
 	size_t excess = block->size - used;
