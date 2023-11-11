0c05f38a1d7c5c480d55e7f577175404a48a3cb7
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-11-11 11:23:39 -0300

    lone: delete duplicated SPDX license identifier
    Behold the power of sed.

--- source/lone.c
+++ source/lone.c
@@ -1,7 +1,5 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 
-/* SPDX-License-Identifier: AGPL-3.0-or-later */
-
 /* ╭─────────────────────────────┨ LONE LISP ┠──────────────────────────────╮
    │                                                                        │
    │                       The standalone Linux Lisp                        │
