633c539b4ed8ac8210f6d7c8ee773ebfae08745c
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-11-16 04:13:28 -0300

    lone: typedef the kernel off_t type
    Will be using it for lseek.

--- include/lone/types.h
+++ include/lone/types.h
@@ -10,6 +10,7 @@
 
 typedef __kernel_size_t size_t;
 typedef __kernel_ssize_t ssize_t;
+typedef __kernel_off_t off_t;
 
 /* ╭──────────────────────────┨ LONE LISP TYPES ┠───────────────────────────╮
    │                                                                        │
