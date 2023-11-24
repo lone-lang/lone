e59a885bab92687617ab84c81de022be8e2dccfc
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-11-23 22:28:46 -0300

    lone: extract slice form parser into function
    Will make it easier to extract slices
    out of the lone segment in the future.

--- source/lone/modules/embedded.c
+++ source/lone/modules/embedded.c
@@ -11,12 +11,36 @@
 #include <lone/value/table.h>
 #include <lone/value/symbol.h>
 
+static struct lone_bytes slice(struct lone_bytes bytes, struct lone_value *pair)
+{
+	struct lone_value *first, *second;
+	size_t start, end, size;
+
+	if (!lone_is_list(pair)) { /* unexpected value type */ linux_exit(-1); }
+	first = lone_list_first(pair);
+	if (!lone_is_integer(first)) { /* unexpected value type */ linux_exit(-1); }
+	second = lone_list_rest(pair);
+	if (!lone_is_integer(second)) { /* unexpected value type */ linux_exit(-1); }
+
+	start = first->integer;
+	size = second->integer;
+	end = start + size;
+
+	if (start >= bytes.count || end >= bytes.count) {
+		/* segment overrun */ linux_exit(-1);
+	}
+
+	return (struct lone_bytes) {
+		.count = size,
+		.pointer = bytes.pointer + start
+	};
+}
+
 void lone_modules_embedded_load(struct lone_lisp *lone, lone_elf_segment *segment)
 {
 	struct lone_value *descriptor = lone_segment_read_descriptor(lone, segment);
-	struct lone_value *module, *symbol, *data, *locations, *first, *rest;
+	struct lone_value *module, *symbol, *data, *locations;
 	struct lone_bytes bytes, code;
-	size_t start, end;
 
 	if (!descriptor) { /* nothing to load */ return; }
 
@@ -26,20 +50,8 @@ void lone_modules_embedded_load(struct lone_lisp *lone, lone_elf_segment *segmen
 
 	symbol = lone_intern_c_string(lone, "run");
 	locations = lone_table_get(lone, descriptor, symbol);
-	if (!lone_is_list(locations)) { /* unexpected value type */ linux_exit(-1); }
-	first = lone_list_first(locations);
-	if (!lone_is_integer(first)) { /* unexpected value type */ linux_exit(-1); }
-	rest = lone_list_rest(locations);
-	if (!lone_is_integer(rest)) { /* unexpected value type */ linux_exit(-1); }
 
-	start = first->integer;
-	end = rest->integer;
-
-	if (start >= bytes.count || end >= bytes.count) {
-		/* segment overrun */ linux_exit(-1);
-	}
-
-	code = (struct lone_bytes) { .count = end - start, .pointer = bytes.pointer + start };
+	code = slice(data->bytes, locations);
 
 	module = lone_module_for_name(lone, symbol);
 	lone_module_load_from_bytes(lone, module, code);
