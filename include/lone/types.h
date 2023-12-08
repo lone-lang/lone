46cb650e6ea0d6521fd4eabddea466f5ce711d76
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-12-08 09:22:37 -0300

    lone/types: convert lone values into double words
    Instead of folding values into pointers, they will now be two words:
    a value word and a metadata word.
    
    This approach is flexible, extensible, understood by debuggers
    and easier to develop since there are less pointers involved.
    It also allows use of a full register for immediate values instead
    of limiting their range and supports further optimizations
    such as compact homogeneous collections.
    
    This comes at the cost of requiring two registers instead of one
    but it seems performance is competitive with tagged pointers
    if internet discussions are to be believed. For now, I believe
    the benefits outweigh the costs.
    
    References:
    
    https://softwareengineering.stackexchange.com/q/448875
    https://old.reddit.com/r/ProgrammingLanguages/comments/186nai0/tagged_pointers_vs_fat_pointers/

--- include/lone/types.h
+++ include/lone/types.h
@@ -58,41 +58,6 @@ struct lone_bytes {
 	unsigned char *pointer;
 };
 
-/* ╭───────────────────────┨ LONE LISP INTERPRETER ┠────────────────────────╮
-   │                                                                        │
-   │    The lone lisp interpreter is composed of all internal state         │
-   │    necessary to process useful programs. It includes memory,           │
-   │    references to all allocated objects, a table of interned            │
-   │    symbols, references to constant values such as nil and              │
-   │    a table of loaded modules and the top level null module.            │
-   │                                                                        │
-   ╰────────────────────────────────────────────────────────────────────────╯ */
-
-struct lone_lisp {
-	struct {
-		void *stack;
-		struct lone_memory *general;
-		struct lone_heap *heaps;
-	} memory;
-	struct lone_value *symbol_table;
-	struct {
-		struct lone_value *nil;
-		struct lone_value *truth;
-	} constants;
-	struct {
-		struct lone_value *loaded;
-		struct lone_value *embedded;
-		struct lone_value *null;
-		struct lone_value *top_level_environment;
-		struct lone_value *path;
-	} modules;
-	struct {
-		struct {
-			unsigned long offset_basis;
-		} fnv_1a;
-	} hash;
-};
-
 struct lone_reader {
 	int file_descriptor;
 	struct {
@@ -124,7 +89,14 @@ struct lone_reader {
    │                                                                        │
    ╰────────────────────────────────────────────────────────────────────────╯ */
 
-enum lone_type {
+enum lone_value_type {
+	LONE_NIL = 0,
+	LONE_HEAP_VALUE,
+	LONE_INTEGER,
+	LONE_POINTER,
+};
+
+enum lone_heap_value_type {
 	LONE_MODULE,
 	LONE_FUNCTION,
 	LONE_PRIMITIVE,
@@ -134,8 +106,6 @@ enum lone_type {
 	LONE_SYMBOL,
 	LONE_TEXT,
 	LONE_BYTES,
-	LONE_POINTER,
-	LONE_INTEGER,
 };
 
 enum lone_pointer_type {
@@ -147,10 +117,55 @@ enum lone_pointer_type {
 	LONE_TO_U64, LONE_TO_I64,
 };
 
+union lone_pointer {
+	void *to_void;
+	char *to_char;
+
+	float  *to_float;
+	double *to_double;
+
+	signed char        *to_signed_char;
+	signed short       *to_signed_short;
+	signed int         *to_signed_int;
+	signed long        *to_signed_long;
+	signed long long   *to_signed_long_long;
+
+	unsigned char      *to_unsigned_char;
+	unsigned short     *to_unsigned_short;
+	unsigned int       *to_unsigned_int;
+	unsigned long      *to_unsigned_long;
+	unsigned long long *to_unsigned_long_long;
+
+	lone_s8  *to_s8;
+	lone_s16 *to_s16;
+	lone_s32 *to_s32;
+	lone_s64 *to_s64;
+
+	lone_u8  *to_u8;
+	lone_u16 *to_u16;
+	lone_u32 *to_u32;
+	lone_u64 *to_u64;
+};
+
+struct lone_heap_value;
+struct lone_value {
+	union {
+		struct lone_heap_value *heap_value;
+		union lone_pointer pointer;
+		signed long signed_integer;
+		unsigned long unsigned_integer;
+	} as;
+
+	struct {
+		enum lone_value_type type;
+		enum lone_pointer_type pointer_type;
+	};
+};
+
 struct lone_module {
-	struct lone_value *name;
-	struct lone_value *environment;
-	struct lone_value *exports;
+	struct lone_value name;
+	struct lone_value environment;
+	struct lone_value exports;
 };
 
 /* https://dl.acm.org/doi/10.1145/947941.947948
@@ -162,32 +177,33 @@ struct lone_function_flags {
 };
 
 struct lone_function {
-	struct lone_value *arguments;        /* the bindings */
-	struct lone_value *code;             /* the lambda */
-	struct lone_value *environment;      /* the closure */
+	struct lone_value arguments;         /* the bindings */
+	struct lone_value code;              /* the lambda */
+	struct lone_value environment;       /* the closure */
 	struct lone_function_flags flags;    /* how to evaluate & apply */
 };
 
-typedef struct lone_value *(*lone_primitive)(struct lone_lisp *lone,
-                                             struct lone_value *module,
-                                             struct lone_value *environment,
-                                             struct lone_value *arguments,
-                                             struct lone_value *closure);
+struct lone_lisp;
+typedef struct lone_value (*lone_primitive)(struct lone_lisp *lone,
+                                            struct lone_value module,
+                                            struct lone_value environment,
+                                            struct lone_value arguments,
+                                            struct lone_value closure);
 
 struct lone_primitive {
-	struct lone_value *name;
+	struct lone_value name;
 	lone_primitive function;
-	struct lone_value *closure;
+	struct lone_value closure;
 	struct lone_function_flags flags;
 };
 
 struct lone_list {
-	struct lone_value *first;
-	struct lone_value *rest;
+	struct lone_value first;
+	struct lone_value rest;
 };
 
 struct lone_vector {
-	struct lone_value **values;
+	struct lone_value *values;
 	size_t count;
 	size_t capacity;
 };
@@ -222,8 +238,8 @@ struct lone_table_index {
 };
 
 struct lone_table_entry {
-	struct lone_value *key;
-	struct lone_value *value;
+	struct lone_value key;
+	struct lone_value value;
 };
 
 struct lone_table {
@@ -231,22 +247,17 @@ struct lone_table {
 	size_t capacity;
 	struct lone_table_index *indexes;
 	struct lone_table_entry *entries;
-	struct lone_value *prototype;
+	struct lone_value prototype;
 };
 
-struct lone_pointer {
-	enum lone_pointer_type type;
-	void *address;
-};
-
-struct lone_value {
+struct lone_heap_value {
 	struct {
 		bool live: 1;
 		bool marked: 1;
 		bool should_deallocate_bytes: 1;
 	};
 
-	enum lone_type type;
+	enum lone_heap_value_type type;
 
 	union {
 		struct lone_module module;
@@ -256,9 +267,7 @@ struct lone_value {
 		struct lone_vector vector;
 		struct lone_table table;
 		struct lone_bytes bytes;   /* also used by texts and symbols */
-		struct lone_pointer pointer;
-		long integer;
-	};
+	} as;
 };
 
 typedef bool (*lone_predicate)(struct lone_value *);
@@ -303,6 +312,40 @@ bool lone_integer_is_greater_than(struct lone_value *x, struct lone_value *y);
 bool lone_integer_is_greater_than_or_equal_to(struct lone_value *x, struct lone_value *y);
 bool lone_bytes_equals(struct lone_bytes x, struct lone_bytes y);
 bool lone_bytes_equals_c_string(struct lone_bytes bytes, char *c_string);
+
+/* ╭───────────────────────┨ LONE LISP INTERPRETER ┠────────────────────────╮
+   │                                                                        │
+   │    The lone lisp interpreter is composed of all internal state         │
+   │    necessary to process useful programs. It includes memory,           │
+   │    references to all allocated objects, a table of interned            │
+   │    symbols, references to constant values such as nil and              │
+   │    a table of loaded modules and the top level null module.            │
+   │                                                                        │
+   ╰────────────────────────────────────────────────────────────────────────╯ */
+
+struct lone_lisp {
+	struct {
+		void *stack;
+		struct lone_memory *general;
+		struct lone_heap *heaps;
+	} memory;
+	struct lone_value symbol_table;
+	struct {
+		struct lone_value truth;
+	} constants;
+	struct {
+		struct lone_value loaded;
+		struct lone_value embedded;
+		struct lone_value null;
+		struct lone_value top_level_environment;
+		struct lone_value path;
+	} modules;
+	struct {
+		struct {
+			unsigned long offset_basis;
+		} fnv_1a;
+	} hash;
+};
 
 /* ╭────────────────────┨ LONE LISP MEMORY ALLOCATION ┠─────────────────────╮
    │                                                                        │
@@ -352,7 +395,7 @@ struct lone_memory {
 struct lone_heap {
 	struct lone_heap *next;
 	size_t count;
-	struct lone_value values[];
+	struct lone_heap_value values[];
 };
 
 #endif /* LONE_TYPES_HEADER */
