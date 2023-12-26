aaffc1a017f4e9560926478349a7051486c51e94
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-12-26 08:01:12 -0300

    test: add simple system call test
    The most basic test imaginable.

---
+++ source/tests/system-call.c
@@ -0,0 +1,9 @@
+#include <lone/architecture/linux/entry_point.c>
+#include <lone/linux.h>
+
+long lone(int argc, char **argv, char **envp, struct lone_auxiliary_vector *auxvec)
+{
+	char message[] = "Hello, world!\n";
+	linux_system_call_3(__NR_write, 1, (long) message, sizeof(message) - 1);
+	return 0;
+}
