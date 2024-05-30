5915d2b89f7e6cea606ccd53c57f62c6e19575b0
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2024-05-30 19:49:43 -0300

    bytes: define native integer read/write functions
    Read and write integers of specified size and signedness
    from the given lone bytes value, starting from the offset.
    Native endianness is assumed, functions that specify endianness
    as well by be added later. Also omit 64 bit integers for now
    due to bugs which surface as platform limits are reached.
    Lone will eventually have infinite precision integer support
    which will make it safe to add even more functions.

--- include/lone/modules/intrinsic/bytes.h
+++ include/lone/modules/intrinsic/bytes.h
@@ -15,4 +15,18 @@ void lone_modules_intrinsic_bytes_initialize(struct lone_lisp *lone);
 
 LONE_PRIMITIVE(bytes_new);
 
+LONE_PRIMITIVE(bytes_read_u8);
+LONE_PRIMITIVE(bytes_read_s8);
+LONE_PRIMITIVE(bytes_read_u16);
+LONE_PRIMITIVE(bytes_read_s16);
+LONE_PRIMITIVE(bytes_read_u32);
+LONE_PRIMITIVE(bytes_read_s32);
+
+LONE_PRIMITIVE(bytes_write_u8);
+LONE_PRIMITIVE(bytes_write_s8);
+LONE_PRIMITIVE(bytes_write_u16);
+LONE_PRIMITIVE(bytes_write_s16);
+LONE_PRIMITIVE(bytes_write_u32);
+LONE_PRIMITIVE(bytes_write_s32);
+
 #endif /* LONE_MODULES_INTRINSIC_BYTES_HEADER */
--- source/lone/modules/intrinsic/bytes.c
+++ source/lone/modules/intrinsic/bytes.c
@@ -10,6 +10,7 @@
 #include <lone/value/table.h>
 #include <lone/value/symbol.h>
 #include <lone/value/bytes.h>
+#include <lone/value/integer.h>
 
 void lone_modules_intrinsic_bytes_initialize(struct lone_lisp *lone)
 {
@@ -21,6 +22,31 @@ void lone_modules_intrinsic_bytes_initialize(struct lone_lisp *lone)
 
 	primitive = lone_primitive_create(lone, "bytes_new", lone_primitive_bytes_new, module, flags);
 	lone_set_and_export(lone, module, lone_intern_c_string(lone, "new"), primitive);
+
+#define LONE_REGISTER_BYTES_READER_PRIMITIVE(type) \
+	primitive = lone_primitive_create(lone, "bytes_read_" #type, lone_primitive_bytes_read_##type, module, flags); \
+	lone_set_and_export(lone, module, lone_intern_c_string(lone, "read-" #type), primitive)
+
+#define LONE_REGISTER_BYTES_WRITER_PRIMITIVE(type) \
+	primitive = lone_primitive_create(lone, "bytes_write_" #type, lone_primitive_bytes_write_##type, module, flags); \
+	lone_set_and_export(lone, module, lone_intern_c_string(lone, "write-" #type), primitive)
+
+	LONE_REGISTER_BYTES_READER_PRIMITIVE(u8);
+	LONE_REGISTER_BYTES_READER_PRIMITIVE(s8);
+	LONE_REGISTER_BYTES_READER_PRIMITIVE(u16);
+	LONE_REGISTER_BYTES_READER_PRIMITIVE(s16);
+	LONE_REGISTER_BYTES_READER_PRIMITIVE(u32);
+	LONE_REGISTER_BYTES_READER_PRIMITIVE(s32);
+
+	LONE_REGISTER_BYTES_WRITER_PRIMITIVE(u8);
+	LONE_REGISTER_BYTES_WRITER_PRIMITIVE(s8);
+	LONE_REGISTER_BYTES_WRITER_PRIMITIVE(u16);
+	LONE_REGISTER_BYTES_WRITER_PRIMITIVE(s16);
+	LONE_REGISTER_BYTES_WRITER_PRIMITIVE(u32);
+	LONE_REGISTER_BYTES_WRITER_PRIMITIVE(s32);
+
+#undef LONE_REGISTER_BYTES_READER_PRIMITIVE
+#undef LONE_REGISTER_BYTES_WRITER_PRIMITIVE
 
 	lone_table_set(lone, lone->modules.loaded, name, module);
 }
@@ -50,3 +76,99 @@ LONE_PRIMITIVE(bytes_new)
 
 	return lone_bytes_create(lone, allocation);
 }
+
+static void lone_bytes_check_read_arguments(struct lone_value bytes, struct lone_value offset)
+{
+	if (!lone_is_bytes(bytes)) {
+		/* invalid data source: (reader "text" 0), (reader {} 0) */ linux_exit(-1);
+	}
+
+	if (!lone_is_integer(offset)) {
+		/* invalid offset: (reader bytes "invalid"), (reader bytes []) */ linux_exit(-1);
+	}
+}
+
+static void lone_bytes_check_write_arguments(struct lone_value bytes, struct lone_value offset, struct lone_value value)
+{
+	lone_bytes_check_read_arguments(bytes, offset);
+
+	if (!lone_is_integer(value)) {
+		/* invalid value: (writer bytes 0 "invalid"), (writer bytes 0 []) */ linux_exit(-1);
+	}
+}
+
+#define LONE_BYTES_READER_PRIMITIVE(type) \
+LONE_PRIMITIVE(bytes_read_##type) \
+{ \
+	struct lone_value bytes; \
+	struct lone_value offset; \
+\
+	lone_##type type; \
+	bool success; \
+\
+	if (lone_list_destructure(arguments, 2, &bytes, &offset)) { \
+		/* wrong number of arguments */ linux_exit(-1); \
+	} \
+\
+	lone_bytes_check_read_arguments(bytes, offset); \
+\
+	success = lone_bytes_checked_read_##type( \
+		bytes.as.heap_value->as.bytes, \
+		offset.as.unsigned_integer, \
+		&type \
+	); \
+\
+	if (success) { \
+		return lone_integer_create(type); \
+	} else { \
+		linux_exit(-1); \
+	} \
+}
+
+#define LONE_BYTES_WRITER_PRIMITIVE(type) \
+LONE_PRIMITIVE(bytes_write_##type) \
+{ \
+	struct lone_value bytes; \
+	struct lone_value offset; \
+	struct lone_value value; \
+\
+	lone_##type type; \
+	bool success; \
+\
+	if (lone_list_destructure(arguments, 3, &bytes, &offset, &value)) { \
+		/* wrong number of arguments */ linux_exit(-1); \
+	} \
+\
+	lone_bytes_check_write_arguments(bytes, offset, value); \
+\
+	type = (lone_##type) value.as.unsigned_integer; \
+\
+	success = lone_bytes_checked_write_##type( \
+		bytes.as.heap_value->as.bytes, \
+		offset.as.unsigned_integer, \
+		type \
+	); \
+\
+	if (success) { \
+		return lone_integer_create(type); \
+	} else { \
+		linux_exit(-1); \
+	} \
+}
+
+LONE_BYTES_READER_PRIMITIVE(u8)
+LONE_BYTES_READER_PRIMITIVE(s8)
+LONE_BYTES_READER_PRIMITIVE(u16)
+LONE_BYTES_READER_PRIMITIVE(s16)
+LONE_BYTES_READER_PRIMITIVE(u32)
+LONE_BYTES_READER_PRIMITIVE(s32)
+
+LONE_BYTES_WRITER_PRIMITIVE(u8)
+LONE_BYTES_WRITER_PRIMITIVE(s8)
+LONE_BYTES_WRITER_PRIMITIVE(u16)
+LONE_BYTES_WRITER_PRIMITIVE(s16)
+LONE_BYTES_WRITER_PRIMITIVE(u32)
+LONE_BYTES_WRITER_PRIMITIVE(s32)
+
+#undef LONE_BYTES_READER_PRIMITIVE
+#undef LONE_BYTES_WRITER_PRIMITIVE
