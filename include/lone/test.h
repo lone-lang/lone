efc134726add39774ac06bae763b40bd9386541d
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2024-07-14 01:11:14 -0300

    lone/test: define suite of assertion functions
    Define a suite of functions for asserting equality or inequality
    of basic C types. They integrate with the test suite as a whole
    by writing the test's result to the test structure.

--- include/lone/test.h
+++ include/lone/test.h
@@ -101,5 +101,133 @@ void __name(struct lone_test_suite *suite, struct lone_test_case *test)
 
 enum lone_test_result lone_test_suite_run(struct lone_test_suite *suite);
 
-#endif /* LONE_TEST_HEADER */
+bool lone_test_assert(struct lone_test_suite *suite,
+		struct lone_test_case *test, struct lone_test_assertion *assertion);
 
+bool lone_test_assert_true(struct lone_test_suite *suite,
+		struct lone_test_case *test, bool b);
+
+bool lone_test_assert_false(struct lone_test_suite *suite,
+		struct lone_test_case *test, bool b);
+
+bool lone_test_assert_boolean_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, bool x, bool y);
+
+bool lone_test_assert_boolean_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, bool x, bool y);
+
+bool lone_test_assert_char_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, char x, char y);
+
+bool lone_test_assert_char_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, char x, char y);
+
+bool lone_test_assert_signed_char_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed char x, signed char y);
+
+bool lone_test_assert_signed_char_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed char x, signed char y);
+
+bool lone_test_assert_unsigned_char_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned char x, unsigned char y);
+
+bool lone_test_assert_unsigned_char_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned char x, unsigned char y);
+
+bool lone_test_assert_short_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed short x, signed short y);
+
+bool lone_test_assert_short_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed short x, signed short y);
+
+bool lone_test_assert_unsigned_short_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned short x, unsigned short y);
+
+bool lone_test_assert_unsigned_short_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned short x, unsigned short y);
+
+bool lone_test_assert_int_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed int x, signed int y);
+
+bool lone_test_assert_int_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed int x, signed int y);
+
+bool lone_test_assert_unsigned_int_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned int x, unsigned int y);
+
+bool lone_test_assert_unsigned_int_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned int x, unsigned int y);
+
+bool lone_test_assert_long_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed long x, signed long y);
+
+bool lone_test_assert_long_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed long x, signed long y);
+
+bool lone_test_assert_unsigned_long_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned long x, unsigned long y);
+
+bool lone_test_assert_unsigned_long_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned long x, unsigned long y);
+
+bool lone_test_assert_long_long_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed long long x, signed long long y);
+
+bool lone_test_assert_long_long_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, signed long long x, signed long long y);
+
+bool lone_test_assert_unsigned_long_long_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned long long x, unsigned long long y);
+
+bool lone_test_assert_unsigned_long_long_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, unsigned long long x, unsigned long long y);
+
+bool lone_test_assert_s8_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s8 x, lone_s8 y);
+
+bool lone_test_assert_s8_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s8 x, lone_s8 y);
+
+bool lone_test_assert_u8_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u8 x, lone_u8 y);
+
+bool lone_test_assert_u8_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u8 x, lone_u8 y);
+
+bool lone_test_assert_s16_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s16 x, lone_s16 y);
+
+bool lone_test_assert_s16_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s16 x, lone_s16 y);
+
+bool lone_test_assert_u16_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u16 x, lone_u16 y);
+
+bool lone_test_assert_u16_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u16 x, lone_u16 y);
+
+bool lone_test_assert_s32_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s32 x, lone_s32 y);
+
+bool lone_test_assert_s32_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s32 x, lone_s32 y);
+
+bool lone_test_assert_u32_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u32 x, lone_u32 y);
+
+bool lone_test_assert_u32_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u32 x, lone_u32 y);
+
+bool lone_test_assert_s64_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s64 x, lone_s64 y);
+
+bool lone_test_assert_s64_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_s64 x, lone_s64 y);
+
+bool lone_test_assert_u64_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u64 x, lone_u64 y);
+
+bool lone_test_assert_u64_not_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, lone_u64 x, lone_u64 y);
+
+#endif /* LONE_TEST_HEADER */
--- source/lone/test.c
+++ source/lone/test.c
@@ -102,3 +102,148 @@ enum lone_test_result lone_test_suite_run(struct lone_test_suite *suite)
 end:
 	return result;
 }
+
+static bool lone_test_assert_equal(struct lone_test_suite *suite,
+		struct lone_test_case *test, struct lone_test_assertion *assertion)
+{
+	switch (assertion->values_type) {
+	case LONE_TYPES_C_CHAR:
+		return assertion->x.as.plain_char == assertion->y.as.plain_char;
+	case LONE_TYPES_C_SIGNED_CHAR:
+		return assertion->x.as.signed_char == assertion->y.as.signed_char;
+	case LONE_TYPES_C_UNSIGNED_CHAR:
+		return assertion->x.as.unsigned_char == assertion->y.as.unsigned_char;
+
+	case LONE_TYPES_C_SHORT:
+		return assertion->x.as.signed_short == assertion->y.as.signed_short;
+	case LONE_TYPES_C_UNSIGNED_SHORT:
+		return assertion->x.as.unsigned_short == assertion->y.as.unsigned_short;
+
+	case LONE_TYPES_C_INT:
+		return assertion->x.as.signed_int == assertion->y.as.signed_int;
+	case LONE_TYPES_C_UNSIGNED_INT:
+		return assertion->x.as.unsigned_int == assertion->y.as.unsigned_int;
+
+	case LONE_TYPES_C_LONG:
+		return assertion->x.as.signed_long == assertion->y.as.signed_long;
+	case LONE_TYPES_C_UNSIGNED_LONG:
+		return assertion->x.as.unsigned_long == assertion->y.as.unsigned_long;
+
+	case LONE_TYPES_C_LONG_LONG:
+		return assertion->x.as.signed_long_long == assertion->y.as.signed_long_long;
+	case LONE_TYPES_C_UNSIGNED_LONG_LONG:
+		return assertion->x.as.unsigned_long_long == assertion->y.as.unsigned_long_long;
+
+	case LONE_TYPES_C_BOOLEAN:
+		return assertion->x.as.boolean == assertion->y.as.boolean;
+
+	case LONE_TYPES_C_POINTER:
+		return assertion->x.as.pointer == assertion->y.as.pointer;
+	case LONE_TYPES_C_FUNCTION_POINTER:
+		return assertion->x.as.function_pointer == assertion->y.as.function_pointer;
+
+	case LONE_TYPES_C_S8:
+		return assertion->x.as.s8  == assertion->y.as.s8;
+	case LONE_TYPES_C_S16:
+		return assertion->x.as.s16 == assertion->y.as.s16;
+	case LONE_TYPES_C_S32:
+		return assertion->x.as.s32 == assertion->y.as.s32;
+	case LONE_TYPES_C_S64:
+		return assertion->x.as.s64 == assertion->y.as.s64;
+
+	case LONE_TYPES_C_U8:
+		return assertion->x.as.u8  == assertion->y.as.u8;
+	case LONE_TYPES_C_U16:
+		return assertion->x.as.u16 == assertion->y.as.u16;
+	case LONE_TYPES_C_U32:
+		return assertion->x.as.u32 == assertion->y.as.u32;
+	case LONE_TYPES_C_U64:
+		return assertion->x.as.u64 == assertion->y.as.u64;
+
+	case LONE_TYPES_C_UNDEFINED:
+	default:
+		test->result = LONE_TEST_RESULT_ERROR;
+		return false;
+	}
+}
+
+bool lone_test_assert(struct lone_test_suite *suite,
+		struct lone_test_case *test, struct lone_test_assertion *assertion)
+{
+	bool result;
+
+	switch (assertion->type) {
+	case LONE_TEST_ASSERTION_TYPE_EQUAL:
+		result = lone_test_assert_equal(suite, test, assertion);
+		break;
+	case LONE_TEST_ASSERTION_TYPE_NOT_EQUAL:
+		result = !lone_test_assert_equal(suite, test, assertion);
+		break;
+	}
+
+	if (test->result == LONE_TEST_RESULT_PENDING) {
+		test->result = result? LONE_TEST_RESULT_PASSED : LONE_TEST_RESULT_FAILED;
+	}
+
+	return result;
+}
+
+bool lone_test_assert_true(struct lone_test_suite *suite,
+		struct lone_test_case *test, bool b)
+{
+	return lone_test_assert_boolean_equal(suite, test, b, true);
+}
+
+bool lone_test_assert_false(struct lone_test_suite *suite,
+		struct lone_test_case *test, bool b)
+{
+	return lone_test_assert_boolean_equal(suite, test, b, false);
+}
+
+#define LONE_TEST_ASSERTION_FUNCTION_5(name, params_type, assertion_type, value_type, field)       \
+bool lone_test_assert_##name(struct lone_test_suite *suite, struct lone_test_case *test,           \
+		params_type x, params_type y)                                                      \
+{                                                                                                  \
+	struct lone_test_assertion assertion = {                                                   \
+		.type = LONE_TEST_ASSERTION_TYPE_##assertion_type,                                 \
+		.values_type = LONE_TYPES_C_##value_type,                                          \
+		.x.as.field = x,                                                                   \
+		.y.as.field = y,                                                                   \
+	};                                                                                         \
+                                                                                                   \
+	return lone_test_assert(suite, test, &assertion);                                          \
+}
+
+#define LONE_TEST_ASSERTION_FUNCTION(name, params_type, value_type, field)                         \
+LONE_TEST_ASSERTION_FUNCTION_5(name##_equal,     params_type, EQUAL,     value_type, field)        \
+LONE_TEST_ASSERTION_FUNCTION_5(name##_not_equal, params_type, NOT_EQUAL, value_type, field)
+
+LONE_TEST_ASSERTION_FUNCTION(boolean, bool, BOOLEAN, boolean)
+
+LONE_TEST_ASSERTION_FUNCTION(char,                   char,    PLAIN_CHAR,    plain_char)
+LONE_TEST_ASSERTION_FUNCTION(signed_char,     signed char,   SIGNED_CHAR,   signed_char)
+LONE_TEST_ASSERTION_FUNCTION(unsigned_char, unsigned char, UNSIGNED_CHAR, unsigned_char)
+
+LONE_TEST_ASSERTION_FUNCTION(short,            signed short,   SIGNED_SHORT,   signed_short)
+LONE_TEST_ASSERTION_FUNCTION(unsigned_short, unsigned short, UNSIGNED_SHORT, unsigned_short)
+
+LONE_TEST_ASSERTION_FUNCTION(int,            signed int,   SIGNED_INT,   signed_int)
+LONE_TEST_ASSERTION_FUNCTION(unsigned_int, unsigned int, UNSIGNED_INT, unsigned_int)
+
+LONE_TEST_ASSERTION_FUNCTION(long,           signed long,   SIGNED_LONG,   signed_long)
+LONE_TEST_ASSERTION_FUNCTION(unsigned_long, unsigned long, UNSIGNED_LONG, unsigned_long)
+
+LONE_TEST_ASSERTION_FUNCTION(long_long,            signed long long,   SIGNED_LONG_LONG,   signed_long_long)
+LONE_TEST_ASSERTION_FUNCTION(unsigned_long_long, unsigned long long, UNSIGNED_LONG_LONG, unsigned_long_long)
+
+LONE_TEST_ASSERTION_FUNCTION(s8,  lone_s8,  S8,  s8)
+LONE_TEST_ASSERTION_FUNCTION(u8,  lone_u8,  U8,  u8)
+LONE_TEST_ASSERTION_FUNCTION(s16, lone_s16, S16, s16)
+LONE_TEST_ASSERTION_FUNCTION(u16, lone_u16, U16, u16)
+LONE_TEST_ASSERTION_FUNCTION(s32, lone_s32, S32, s32)
+LONE_TEST_ASSERTION_FUNCTION(u32, lone_u32, U32, u32)
+LONE_TEST_ASSERTION_FUNCTION(s64, lone_s64, S64, s64)
+LONE_TEST_ASSERTION_FUNCTION(u64, lone_u64, U64, u64)
+
+#undef LONE_TEST_ASSERTION_FUNCTION
+#undef LONE_TEST_ASSERTION_FUNCTION_5
