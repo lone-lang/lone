465ca97f47b9458e2cb98e16e254d5e9d2e735ba
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2024-07-28 20:00:32 -0300

    lone: create the lone ELF header
    Create a header file for lone's ELF implementation
    which will complement the Linux user space API headers.
    
    This is meant to be a unique ELF implementation,
    based on my understanding of the specification.
    It will have the ability to process arbitrary ELFs.

---
+++ include/lone/elf.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+
+#ifndef LONE_ELF_HEADER
+#define LONE_ELF_HEADER
+
+#include <linux/elf.h>
+
+#include <lone/definitions.h>
+#include <lone/types.h>
+
+/* Latest ELF specification as of this writing:
+ * https://www.sco.com/developers/gabi/latest/contents.html
+ */
+
+#endif /* LONE_ELF_HEADER */
