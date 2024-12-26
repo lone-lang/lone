65295fd042dd1ff05e849039b8a8df3e03bf6f98
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2024-12-26 00:25:08 -0300

    lisp/types: define the boolean value types
    Add a register value type for true and another for false.

--- include/lone/lisp/types.h
+++ include/lone/lisp/types.h
@@ -61,6 +61,8 @@ enum lone_lisp_value_type {
 	LONE_LISP_TYPE_INTEGER    = (0 << 2) | (0 << 1) | (1 << 0), /* ??1 */
 	LONE_LISP_TYPE_HEAP_VALUE = (0 << 2) | (0 << 1) | (0 << 0), /* 000 */
 	LONE_LISP_TYPE_NIL        = (0 << 2) | (1 << 1) | (0 << 1), /* 010 */
+	LONE_LISP_TYPE_FALSE      = (1 << 2) | (0 << 1) | (0 << 1), /* 100 */
+	LONE_LISP_TYPE_TRUE       = (1 << 2) | (1 << 1) | (0 << 1), /* 110 */
 };
 
 struct lone_lisp_value {
