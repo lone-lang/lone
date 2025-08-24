0d2779648dc82e84c0a66a613b568915645f3452
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2025-08-24 06:39:46 -0300

    lisp: use the lisp machine to implement evaluation
    Use the new lone lisp machine to implement
    in-module evaluation of expressions.
    This replaces the old recursive evaluator.

--- source/lone/lisp/evaluator.c
+++ source/lone/lisp/evaluator.c
@@ -1,8 +1,12 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 
 #include <lone/lisp/evaluator.h>
+#include <lone/lisp/machine.h>
 
 struct lone_lisp_value lone_lisp_evaluate_in_module(struct lone_lisp *lone,
 		struct lone_lisp_value module, struct lone_lisp_value value)
 {
+	lone_lisp_machine_reset(lone, module, value);
+	while (lone_lisp_machine_cycle(lone));
+	return lone->machine.value;
 }
