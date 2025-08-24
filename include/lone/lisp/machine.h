96ce36675a230ace380b71b8b0deb55eb21c73d6
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2025-08-24 06:59:53 -0300

    lisp: declare machine stack management functions
    Declare lisp machine stack management helper functions.
    Programming this thing can get  somewhat convoluted.
    These functions make it easier to understand.

--- include/lone/lisp/machine.h
+++ include/lone/lisp/machine.h
@@ -8,4 +8,28 @@
 void lone_lisp_machine_reset(struct lone_lisp *lone, struct lone_lisp_value module, struct lone_lisp_value expression);
 bool lone_lisp_machine_cycle(struct lone_lisp *lone);
 
+void lone_lisp_machine_push(struct lone_lisp *lone, struct lone_lisp_machine *machine,
+		struct lone_lisp_machine_stack_frame frame);
+struct lone_lisp_machine_stack_frame lone_lisp_machine_pop(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+
+void lone_lisp_machine_push_value(struct lone_lisp *lone, struct lone_lisp_machine *machine,
+		struct lone_lisp_value value);
+struct lone_lisp_value lone_lisp_machine_pop_value(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+
+void lone_lisp_machine_push_integer(struct lone_lisp *lone, struct lone_lisp_machine *machine,
+		lone_lisp_integer integer);
+lone_lisp_integer lone_lisp_machine_pop_integer(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+
+void lone_lisp_machine_push_step(struct lone_lisp *lone, struct lone_lisp_machine *machine,
+		enum lone_lisp_machine_step step);
+enum lone_lisp_machine_step lone_lisp_machine_pop_step(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+void lone_lisp_machine_push_primitive_step(struct lone_lisp *lone, struct lone_lisp_machine *machine,
+		long primitive_step);
+long lone_lisp_machine_pop_primitive_step(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+
+void lone_lisp_machine_save_step(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+void lone_lisp_machine_restore_step(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+void lone_lisp_machine_save_primitive_step(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+void lone_lisp_machine_restore_primitive_step(struct lone_lisp *lone, struct lone_lisp_machine *machine);
+
 #endif /* LONE_LISP_MACHINE_HEADER */
