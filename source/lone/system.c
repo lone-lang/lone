4c1322330b8e01485482fc5d0bd085ff39e9b35c
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-03-28 01:18:57 -0300

    lone/system: pass auxiliary vector to initializer
    Obtain the random bytes within the lone system initializer.

--- source/lone/system.c
+++ source/lone/system.c
@@ -3,9 +3,15 @@
 #include <lone/system.h>
 #include <lone/memory.h>
 #include <lone/hash.h>
+#include <lone/auxiliary_vector.h>
 
-void lone_system_initialize(struct lone_system *system, struct lone_bytes initial_static_memory, struct lone_bytes random_bytes)
+void lone_system_initialize(struct lone_system *system, struct lone_auxiliary_vector *auxiliary_vector, struct lone_bytes initial_static_memory
+)
 {
+	struct lone_bytes random_bytes;
+
+	random_bytes = lone_auxiliary_vector_random(auxiliary_vector);
+
 	lone_memory_initialize(system, initial_static_memory);
 	lone_hash_initialize(system, random_bytes);
 }
