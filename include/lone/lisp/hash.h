b0cd74e89f3e8ba68f30bb1b5d22cdfefa1570d4
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-03-30 07:40:27 -0300

    lisp/hash: generalize hash helpers for all types
    Generalize the hash-bytes-as-symbol helper function
    into helpers for every type that contains bytes:
    symbols, texts and bytes lone lisp heap values.

--- include/lone/lisp/hash.h
+++ include/lone/lisp/hash.h
@@ -6,6 +6,9 @@
 #include <lone/lisp/types.h>
 
 size_t lone_lisp_hash(struct lone_lisp *lone, struct lone_lisp_value value);
+
 size_t lone_lisp_hash_as_symbol(struct lone_lisp *lone, struct lone_bytes name);
+size_t lone_lisp_hash_as_text(struct lone_lisp *lone, struct lone_bytes bytes);
+size_t lone_lisp_hash_as_bytes(struct lone_lisp *lone, struct lone_bytes bytes);
 
 #endif /* LONE_LISP_HASH_HEADER */
