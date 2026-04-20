a224f7dbcde3b0f5f9bf751f34d96d3b1f620c00
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-19 23:01:21 -0300

    list: emit type-error if rest is passed non-list
    Mirror the first primitive's type check.
    Emit type-error carrying the offending value
    when the input is neither nil nor a list.

--- source/lone/lisp/modules/intrinsic/list.c
+++ source/lone/lisp/modules/intrinsic/list.c
@@ -89,6 +89,16 @@ LONE_LISP_PRIMITIVE(list_rest)
 		/* wrong number of arguments */ linux_exit(-1);
 	}
 
+	if (!lone_lisp_is_nil(argument) && !lone_lisp_is_list(lone, argument)) {
+		return
+			lone_lisp_signal_cast(
+				lone,
+				machine,
+				lone_lisp_intern_c_string(lone, "type-error"),
+				argument
+			);
+	}
+
 	lone_lisp_machine_push_value(lone, machine, lone_lisp_list_rest(lone, argument));
 
 	return 0;
