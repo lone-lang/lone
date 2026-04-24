f9cb2d08c05b8783d3fbd02e5ac67c887af79725
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-23 23:10:16 -0300

    entry: prefix aarch64 immediates with #
    ARM assembly immediates are canonically written with a # prefix.
    GNU gas and clang both accept bare integer literals, but it's
    still incorrect.
    
    So correct it.

--- architecture/aarch64/include/lone/architecture/linux/entry.c
+++ architecture/aarch64/include/lone/architecture/linux/entry.c
@@ -42,13 +42,13 @@ void lone_entry(void)
 	(
 	                                       // compute argc, argv, envp and auxv
 	"ldr x0, [sp]"                   "\n"  // argc: x0 =   *sp
-	"add x1, sp, 8"                  "\n"  // argv: x1 =    sp + 8
-	"add x2, x0, 1"                  "\n"  //       x2 =  argc + 1
-	"lsl x2, x2, 3"                  "\n"  //       x2 =    x2 * 8
+	"add x1, sp, #8"                 "\n"  // argv: x1 =    sp + 8
+	"add x2, x0, #1"                 "\n"  //       x2 =  argc + 1
+	"lsl x2, x2, #3"                 "\n"  //       x2 =    x2 * 8
 	"add x2, x1, x2"                 "\n"  // envp: x2 =  argv + x2
 	"mov x3, x2"                     "\n"  //       x3 =  envp
 	"0:"                             "\n"  //       null finder loop:
-	"ldr x8, [x3], 8"                "\n"  //       x8 = *x3
+	"ldr x8, [x3], #8"               "\n"  //       x8 = *x3
 	                                       //       x3 =  x3 + 8
 	"cbnz x8, 0b"                    "\n"  //       goto loop if x8 != 0
 	                                       // auxv: x3
@@ -69,8 +69,8 @@ void lone_entry(void)
 #define S2(s) #s
 #define S(s) S2(s)
 
-	"mov x8, " S(__NR_exit)          "\n"  // ensure clean process termination
-	"svc 0"                          "\n"  // exit with returned status code
+	"mov x8, #" S(__NR_exit)         "\n"  // ensure clean process termination
+	"svc #0"                         "\n"  // exit with returned status code
 
 #undef S2
 #undef S
