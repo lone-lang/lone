7612db4c6b2b028228a5bdba25eefbbb108af66e
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-30 22:15:05 -0300

    lone/unicode: add header file
    Lone system header for Unicode operations.
    No dependency on the lisp layer.

---
+++ include/lone/unicode.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+
+#ifndef LONE_UNICODE_HEADER
+#define LONE_UNICODE_HEADER
+
+#include <lone/types.h>
+
+/* ╭──────────────────────────────┨ UNICODE ┠───────────────────────────────╮
+   │                                                                        │
+   │    UTF-8 decoder, encoder and validator.                               │
+   │                                                                        │
+   ╰────────────────────────────────────────────────────────────────────────╯ */
+
+#endif /* LONE_UNICODE_HEADER */
