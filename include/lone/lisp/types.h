b29f98379a3f019f024e6bee772e2e33ef1a9438
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-04-30 22:22:30 -0300

    lisp/heap: add code point count cached flag
    Tracks whether the code point count has been computed and stored
    on the text heap value. Same pattern as the hash_cached flag.

--- include/lone/lisp/types.h
+++ include/lone/lisp/types.h
@@ -327,6 +327,7 @@ struct lone_lisp_heap_value {
 		bool should_deallocate_bytes: 1;
 		bool frozen: 1;
 		bool hash_cached: 1;
+		bool code_point_count_cached: 1;
 	};
 
 	enum lone_lisp_tag type; /* tag byte, set at allocation for GC sweep */
