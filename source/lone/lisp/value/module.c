2ec4024e75265a2833be50b5d6b43ae56e595b38
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-05-13 06:04:25 -0300

    module: create tables before allocating heap value
    Allocating a table may grow the heap via mremap with MAYMOVE
    which invalidates pointers stored in local variables.
    
    Create both environment and exports tables first,
    then allocate the module heap value and assign them.

--- source/lone/lisp/value/module.c
+++ source/lone/lisp/value/module.c
@@ -5,9 +5,20 @@
 
 struct lone_lisp_value lone_lisp_module_create(struct lone_lisp *lone, struct lone_lisp_value name)
 {
-	struct lone_lisp_heap_value *actual = lone_lisp_heap_allocate_value(lone);
-	actual->as.module.name = name;
-	actual->as.module.environment = lone_lisp_table_create(lone, 64, lone->modules.top_level_environment);
-	actual->as.module.exports = lone_lisp_table_create(lone, 16, lone_lisp_nil());
+	struct lone_lisp_heap_value *actual;
+	struct lone_lisp_value environment, exports;
+
+	/* create tables before allocating the module heap value
+	   table_create transitively calls heap_allocate_value
+	   which may grow the heap and invalidate pointers       */
+	environment = lone_lisp_table_create(lone, 64, lone->modules.top_level_environment);
+	exports     = lone_lisp_table_create(lone, 16, lone_lisp_nil());
+
+	actual = lone_lisp_heap_allocate_value(lone);
+
+	actual->as.module.name        = name;
+	actual->as.module.environment = environment;
+	actual->as.module.exports     = exports;
+
 	return lone_lisp_value_from_heap_value(lone, actual, LONE_LISP_TAG_MODULE);
 }
