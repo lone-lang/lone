a6b4f8bf4254cb533db96a1f780e4faa84d79a6c
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2026-06-02 19:57:19 -0300

    table: implement resumable signals in get
    The get primitive now supports signals and continuation recovery.

--- source/lone/lisp/modules/intrinsic/table.c
+++ source/lone/lisp/modules/intrinsic/table.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 
 #include <lone/lisp/modules/intrinsic/table.h>
+#include <lone/lisp/modules/intrinsic/lone.h>
 
 #include <lone/lisp/machine.h>
 #include <lone/lisp/machine/stack.h>
@@ -38,13 +39,70 @@ LONE_LISP_PRIMITIVE(table_get)
 {
 	struct lone_lisp_value arguments, table, key, value;
 
-	arguments = lone_lisp_machine_pop_value(lone, machine);
+	switch (step) {
+	case 0:
 
-	if (lone_lisp_list_destructure(lone, arguments, 2, &table, &key)) {
-		/* wrong number of arguments */ linux_exit(-1);
+		arguments = lone_lisp_machine_pop_value(lone, machine);
+
+		goto destructure;
+
+	case 1: /* resumed with replacement argument list */
+
+		arguments = machine->value;
+
+		if (!lone_lisp_is_list(lone, arguments)) {
+			/* cannot destructure */
+			return
+				lone_lisp_signal_emit(
+					lone,
+					machine,
+					1,
+					lone->symbols.tags.type_error,
+					arguments
+				);
+		}
+
+		goto destructure;
+
+	case 2: /* resumed with replacement table from type-error */
+
+		key   = lone_lisp_machine_pop_value(lone, machine);
+		table = machine->value;
+
+		goto check_table;
+
+	default:
+		__builtin_trap();
 	}
 
-	if (!lone_lisp_is_table(lone, table)) { /* table not given: (get []) */ linux_exit(-1); }
+destructure:
+
+	if (lone_lisp_list_destructure(lone, arguments, 2, &table, &key)) {
+		/* wrong number of arguments: (get), (get {} 0 "extra") */
+		return
+			lone_lisp_signal_emit(
+				lone,
+				machine,
+				1,
+				lone->symbols.tags.arity_error,
+				arguments
+			);
+	}
+
+check_table:
+
+	if (!lone_lisp_is_table(lone, table)) {
+		/* table not given: (get []) */
+		lone_lisp_machine_push_value(lone, machine, key);
+		return
+			lone_lisp_signal_emit(
+				lone,
+				machine,
+				2,
+				lone->symbols.tags.type_error,
+				table
+			);
+	}
 
 	value = lone_lisp_table_get(lone, table, key);
 
